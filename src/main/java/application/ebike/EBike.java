a22a345d3051eeec2960f410bee2a6da4b56f0c0
Victor Nguyen <hoangloi669@gmail.com>
2020-06-02 12:09:32 +0700

    create entity relationship

--- src/main/java/application/ebike/EBike.java
+++ src/main/java/application/ebike/EBike.java
@@ -3,11 +3,14 @@ package application.ebike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-public class DemoApplication {
+@EnableSwagger2
+public class EBike {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EBike.class, args);
 	}
 
 }
