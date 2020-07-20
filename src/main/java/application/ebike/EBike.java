4714c217ea0cbeb24b0b4fc00564dc509cc6b45f
Victor Nguyễn <hoangloi669@gmail.com>
2020-07-20 12:47:50 +0700

    enable for global cors

--- src/main/java/application/ebike/EBike.java
+++ src/main/java/application/ebike/EBike.java
@@ -3,11 +3,24 @@ package application.ebike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class EBike {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EBike.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }
--- src/main/java/application/ebike/controller/ElementorController.java
+++ src/main/java/application/ebike/controller/ElementorController.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +20,6 @@ public class ElementorController {
     @Autowired
     private ElementorService elementorService;
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
     public Collection<Elementor> getElementors() {
 
